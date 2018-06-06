7923a98c30700f41c2e68755c1b491dbe55c228d
ypuskar <ypuskar@gmail.com>
2018-06-06 22:31:53 +0300

    First commit

---
+++ app.old.js
@@ -0,0 +1,253 @@
+'use strict';
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+//var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var clientsRouter = require('./routes/routes');
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
+var bodyParser = require('body-parser');
+var methodOverride = require('express-method-override');
+var passport = require('passport');
+var util = require('util');
+var bunyan = require('bunyan');
+var config = require('./config');
+var compression = require('compression');
+var helmet = require('helmet');
+
+// Start QuickStart here
+
+var OIDCStrategy = require('passport-azure-ad').OIDCStrategy;
+
+var log = bunyan.createLogger({
+    name: 'Microsoft OIDC Example Web Application'
+});
+
+
+var app = express();
+app.use(compression()); //Compress all routes
+app.use(helmet()); //use protection against vulnerabilities
+
+/******************************************************************************
+ * Set up passport in the app
+ ******************************************************************************/
+
+//-----------------------------------------------------------------------------
+// To support persistent login sessions, Passport needs to be able to
+// serialize users into and deserialize users out of the session.  Typically,
+// this will be as simple as storing the user ID when serializing, and finding
+// the user by ID when deserializing.
+//-----------------------------------------------------------------------------
+passport.serializeUser(function(user, done) {
+  done(null, user.oid);
+});
+
+passport.deserializeUser(function(oid, done) {
+  findByOid(oid, function (err, user) {
+    done(err, user);
+  });
+});
+// array to hold logged in users
+var users = [];
+
+var findByOid = function(oid, fn) {
+  for (var i = 0, len = users.length; i < len; i++) {
+    var user = users[i];
+   log.info('we are using user: ', user);
+    if (user.oid === oid) {
+      return fn(null, user);
+    }
+  }
+  return fn(null, null);
+};
+passport.use(new OIDCStrategy({
+    identityMetadata: config.creds.identityMetadata,
+    clientID: config.creds.clientID,
+    responseType: config.creds.responseType,
+    responseMode: config.creds.responseMode,
+    redirectUrl: config.creds.redirectUrl,
+    allowHttpForRedirectUrl: config.creds.allowHttpForRedirectUrl,
+    clientSecret: config.creds.clientSecret,
+    validateIssuer: config.creds.validateIssuer,
+    isB2C: config.creds.isB2C,
+    issuer: config.creds.issuer,
+    passReqToCallback: config.creds.passReqToCallback,
+    scope: config.creds.scope,
+    loggingLevel: config.creds.loggingLevel,
+    nonceLifetime: config.creds.nonceLifetime,
+    nonceMaxAmount: config.creds.nonceMaxAmount,
+    useCookieInsteadOfSession: config.creds.useCookieInsteadOfSession,
+    cookieEncryptionKeys: config.creds.cookieEncryptionKeys,
+    clockSkew: config.creds.clockSkew,
+  },
+  function(req, iss, sub, profile, accessToken, refreshToken, done) {
+    if (!profile.oid) {
+      return done(new Error("No oid found"), null);
+    }
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+      findByOid(profile.oid, function(err, user) {
+        if (err) {
+          return done(err);
+        }
+        if (!user) {
+          // "Auto-registration"
+          users.push(profile);
+          console.log("USER EI LEITUD");
+          return done(null, profile);
+        }
+        console.log(user);
+        return done(null, user);
+      });
+    });
+  }
+));
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'public'), {dotfiles: 'allow'}));
+
+app.use(methodOverride());
+app.use(cookieParser());
+
+// set up session middleware
+if (config.useMongoDBSessionStore) {
+  mongoose.connect(config.databaseUri);
+  app.use(express.session({
+    secret: 'secret',
+    cookie: {maxAge: config.mongoDBSessionMaxAge * 1000},
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      clear_interval: config.mongoDBSessionMaxAge
+    })
+  }));
+} else {
+  app.use(expressSession({ secret: 'keyboard cat', resave: true, saveUninitialized: false }));
+}
+
+app.use(bodyParser.urlencoded({ extended : true }));
+// Initialize Passport!  Also use passport.session() middleware, to support
+// persistent login sessions (recommended).
+app.use(passport.initialize());
+app.use(passport.session());
+//var router = express.Router();
+//-----------------------------------------------------------------------------
+// Set up the route controller
+//
+// 1. For 'login' route and 'returnURL' route, use `passport.authenticate`.
+// This way the passport middleware can redirect the user to login page, receive
+// id_token etc from returnURL.
+//
+// 2. For the routes you want to check if user is already logged in, use
+// `ensureAuthenticated`. It checks if there is an user stored in session, if not
+// it will call `passport.authenticate` to ask for user to log in.
+//-----------------------------------------------------------------------------
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login');
+};
+
+
+
+app.get('/',  function(req, res) {
+  if(!req.user) {
+  res.render('index', { title: 'Feb kliendirakendusse sisenemine', user: req.user });
+} else {
+  res.redirect('/clients/customers/customers');
+}
+});
+
+app.get('/login',
+  function(req, res, next) {
+
+    passport.authenticate('azuread-openidconnect',
+      {
+//        response: res,                      // required
+        resourceURL: config.resourceURL,    // optional. Provide a value if you want to specify the resource.
+        customState: 'my_state',            // optional. Provide a value if you want to provide custom state value.
+        failureRedirect: '/'
+      }
+    )(req, res, next);
+  },
+  function(req, res) {
+    log.info('Login was called in the Sample');
+    res.redirect('/');
+});
+
+
+// 'GET returnURL'
+// `passport.authenticate` will try to authenticate the content returned in
+// query (such as authorization code). If authentication fails, user will be
+// redirected to '/' (home page); otherwise, it passes to the next middleware.
+app.get('/auth/openid/return',
+  function(req, res, next) {
+    passport.authenticate('azuread-openidconnect',
+      {
+        response: res,                      // required
+        failureRedirect: '/'
+      }
+    )(req, res, next);
+  },
+  function(req, res) {
+    log.info('We received a return from AzureAD.');
+    res.redirect('/');
+  });
+
+// 'POST returnURL'
+// `passport.authenticate` will try to authenticate the content returned in
+// body (such as authorization code). If authentication fails, user will be
+// redirected to '/' (home page); otherwise, it passes to the next middleware.
+app.post('/auth/openid/return',
+  function(req, res, next) {
+    passport.authenticate('azuread-openidconnect',
+      {
+        response: res,                      // required
+        failureRedirect: '/'
+      }
+    )(req, res, next);
+  },
+  function(req, res) {
+    log.info('We received a return from AzureAD.');
+    res.redirect('/');
+  });
+
+// 'logout' route, logout from passport, and destroy the session with AAD.
+app.get('/logout', function(req, res){
+  req.session.destroy(function(err) {
+    req.logOut();
+    res.redirect(config.destroySessionUrl);
+  });
+});
+
+
+app.use('/users', usersRouter);
+//app.use('/clients', clientsRouter);
+//app.use('/clients/json', clientsRouter);
+app.use('/clients/customers',  clientsRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/routes.js
@@ -0,0 +1,22 @@
+//var Joi = require('joi');
+var express = require('express');
+var router = express.Router();
+var appfn = require('../app');
+var Handlers = require('../handlers/handlers');
+var DataStore = require('../datastore/dataStore');
+var Customer = require('../module/customers');
+var Boom = require('boom');
+
+
+
+
+
+router.get('/', Handlers.customer_list);
+router.get('/json/', Handlers.customer_json);
+router.get('/customers/', Handlers.customer_feb);
+router.post('/customers/:tekstId/update', Handlers.customer_feb_tekst);
+router.get('/customers/:kommId/komm', Handlers.kommentaarid_json);
+router.get('/customers/:kommId/arve', Handlers.arved_json);
+
+
+module.exports = router;
